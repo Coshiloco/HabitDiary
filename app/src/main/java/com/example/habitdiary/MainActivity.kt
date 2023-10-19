12a8a1403086d09340f3c4c6870466b1dc75bdc4
Elias <elias.brea1137@gmail.com>
2023-10-20 00:22:30 +0100

    HAB-1 Hola mundo de prueba

--- app/src/main/java/com/example/habitdiary/MainActivity.kt
+++ app/src/main/java/com/example/habitdiary/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Greeting("Hola Mundo")
                 }
             }, colorScheme = MaterialTheme.colorScheme)
         }
