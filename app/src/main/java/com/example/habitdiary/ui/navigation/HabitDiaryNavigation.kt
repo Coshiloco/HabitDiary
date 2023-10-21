881e04190ad45207d3c08c72491bfb4c2becb989
Coshiloco <pablohurtadohg@gmail.com>
2023-10-21 02:30:26 +0200

    HAB-1 Learning Navigation and packages

---
+++ app/src/main/java/com/example/habitdiary/ui/navigation/HabitDiaryNavigation.kt
@@ -0,0 +1,34 @@
+package com.example.habitdiary.ui.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.habitdiary.ui.screens.login.WelcomeScreen
+import com.google.accompanist.navigation.material.ExperimentalMaterialNavigationApi
+import com.google.accompanist.navigation.material.ModalBottomSheetLayout
+import com.google.accompanist.navigation.material.rememberBottomSheetNavigator
+
+
+@OptIn(ExperimentalMaterialNavigationApi::class)
+@Composable
+fun HabitDiaryNavigation() {
+
+    val bottomSheetNavigator = rememberBottomSheetNavigator()
+    val navController = rememberNavController(bottomSheetNavigator)
+
+    ModalBottomSheetLayout(bottomSheetNavigator) {
+        NavHost(
+            navController = navController,
+            startDestination = HabitDiaryRoutes.WelcomeScreen.route
+        ) {
+            composable(HabitDiaryRoutes.WelcomeScreen.route) {
+                WelcomeScreen(navController = navController)
+            }
+            composable(HabitDiaryRoutes.HomeScreen.route) {
+                
+            }
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/habitdiary/ui/screens/login/WelcomeScreen.kt
@@ -0,0 +1,26 @@
+package com.example.habitdiary.ui.screens.login
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+
+@Composable
+fun WelcomeScreen(
+    navController: NavController
+) {
+    WelcomeContent()
+}
+
+
+@Composable
+fun WelcomeContent() {
+
+}
+
+@Preview
+@Composable
+fun WelcomeScreenPreview() {
+    WelcomeContent()
+}
+
+
