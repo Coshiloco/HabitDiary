9e46f44970a76f5efc360aae338043429cf658fd
Coshiloco <pablohurtadohg@gmail.com>
2023-10-24 00:13:45 +0200

    HAB-1 Fix Theme

--- app/src/main/java/com/example/habitdiary/ui/theme/Theme.kt
+++ app/src/main/java/com/example/habitdiary/ui/theme/Theme.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.ColorScheme
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
@@ -115,7 +116,9 @@ fun HabitDiaryTheme(
         }
     }
 
-    HabitDiaryTheme(
+    MaterialTheme(
+        colorScheme = colorScheme,
+        typography = Typography,
         content = content,
     )
 }
