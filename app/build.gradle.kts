d3fecf6c44d6806cb9b34916b01f2337f186fc65
Coshiloco <pablohurtadohg@gmail.com>
2023-10-24 00:06:47 +0200

    HAB-1 Update Gradle Two

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,7 +34,7 @@ android {
         targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_17
+        jvmTarget = JavaVersion.VERSION_17.toString()
     }
     buildFeatures {
         compose = true
